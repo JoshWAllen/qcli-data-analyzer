2ba98c8f2e7ab1ceb68139811520a9135f9e1e97
Josh <jwallen883@gmail.com>
2023-07-28 15:59:50 -0400

    Added basic header and sidebar to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,50 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import {
+		AppBar,
+		AppRail,
+		AppRailAnchor,
+		AppRailTile,
+		AppShell,
+		LightSwitch
+	} from '@skeletonlabs/skeleton';
+
+	let currentTile = 0;
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar class="w-full">
+			<svelte:fragment slot="lead">
+				<h1>QCLI</h1>
+			</svelte:fragment>
+			<h1>Current Tile: {currentTile}</h1>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<hr class="opacity-30" />
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 1</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 2</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 3</span>
+			</AppRailTile>
+		</AppRail>
+	</svelte:fragment>
+	<slot />
+</AppShell>
