ab4fd5a4ada1907d48235ff997453e801c786bd8
Josh <jwallen883@gmail.com>
2023-07-28 23:12:19 -0400

    implemented basic table with hardcoded data

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { FileDropzone } from '@skeletonlabs/skeleton';
+	import { FileDropzone, Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
 	import { parse } from 'papaparse';
 	let files: FileList;
 
@@ -15,6 +15,24 @@
 			}
 		});
 	}
+
+	const sourceData = [
+		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
+	];
+	const tableSimple: TableSource = {
+		// A list of heading labels.
+		head: ['Name', 'Symbol', 'Weight'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+		// Optional: A list of footer labels.
+		foot: ['Total', '', '<code class="code">5 Rows</code>']
+	};
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -25,5 +43,6 @@
 			<svelte:fragment slot="message">Upload a file or drag and drop</svelte:fragment>
 			<svelte:fragment slot="meta">only CSV allowed</svelte:fragment>
 		</FileDropzone>
+		<Table source={tableSimple} />
 	</div>
 </div>
